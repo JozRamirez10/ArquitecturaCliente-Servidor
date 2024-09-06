b1e5dc12a86add59101ee7a18f988e0c9bc321e5
Joz Fama <jm_rg@comunidad.unam.mx>
2024-09-06 09:32:11 -0600

    Comentarios de la tarea 2

--- Tema1/programa24_fork-exec.c
+++ Tema1/programa24_fork-exec.c
@@ -1,46 +1,45 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red255\green255\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-#include <stdio.h>\
-#include <stdlib.h>\
-#include <sys/types.h>\
-#include <unistd.h>\
-\
-int spawn (char* program, char** arg_list)\
-\{\
- pid_t child_pid;\
- child_pid = fork ();\
- if (child_pid != 0)\
-  return child_pid;\
- else\
- \{\
-  // RECUERDEN: Si execvp() se ejecuta exitosamente\
-  // el proceso "parent" termina en ese momento,\
-  // pero termina con exito\
-  execvp (program, arg_list);\
-  printf ("ocurrio un error al ejecutar execvp(...)\\n");\
-  // alternativa al printf:\
-  // fprintf (stderr, "ocurrio un error al ejecutar execvp(...)\\n");\
-  return 1; // termina con error\
- \}\
-\}\
-\
-int main ()\
-\{\
-  char* arg_list[] = \{\
-   "ls",\
-   "-l",\
-   "/",\
-   NULL\
-  \};\
-\
-  spawn ("ls", arg_list);\
-  printf ("Termina el proceso padre\\n");\
-  return 0; // Termina con exito\
-\}}
+// Esta funcion engendra al proceso hijo
+// program: programa a ejecutar
+// arg_list: lista de argumentos para la ejecucion del proceso hijo
+int spawn (char* program, char** arg_list)
+{
+ // Variable del proceso hijo
+ pid_t child_pid;
+ child_pid = fork(); // Con el fork creamos el proceso hijo
+ if (child_pid != 0) // El padre entra en este if
+  return child_pid; // El padre regresa a la funcion principal
+ else // El proceso hijo entra aqui
+ {
+  // RECUERDEN: Si execvp() se ejecuta exitosamente
+  // el proceso "parent" termina en ese momento,
+  // pero termina con exito
+  execvp (program, arg_list); // El proceso hijo es remplazado por execvp
+    // Si el proceso se ejecuta correctamente, hasta aqui llega la funcion
+  printf ("ocurrio un error al ejecutar execvp(...)\n");
+  // alternativa al printf:
+  // fprintf (stderr, "ocurrio un error al ejecutar execvp(...)\\n");
+  return 1; // termina con error
+ }
+}
+
+// Funcion principal
+int main ()
+{
+  // Lista de argumentos para la ejecucion de execvp
+  char* arg_list[] = {
+   "ls",
+   "-l",
+   "/",
+   NULL
+  };
+
+  // Llama a la funcion para engendrar un hijo y ejecutar un proceso
+  spawn ("ls", arg_list);
+  printf ("Termina el proceso padre\n"); // Solo el padre llega a esta linea
+  return 0; // Termina con exito
+}
